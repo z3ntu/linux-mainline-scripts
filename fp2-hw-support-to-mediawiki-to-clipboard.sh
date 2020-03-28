fa857278e88c0e909d556ef01c265d26644a6cff
Luca Weiss <luca@z3ntu.xyz>
2020-03-28 18:52:21 +0100

    Update wiki script

--- fp2-hw-support-to-mediawiki-to-clipboard.sh
+++ fp2-hw-support-to-mediawiki-to-clipboard.sh
@@ -3,4 +3,5 @@
 pandoc fp2-hw-support.md --from gfm --to mediawiki |
     sed 's/{|/{| class="wikitable"/' |
     sed 's|{DSP}|<ref name="DSP"/>|' |
+    sed 's|{CCI}|<ref name="CCI"/>|' |
     xsel --clipboard
