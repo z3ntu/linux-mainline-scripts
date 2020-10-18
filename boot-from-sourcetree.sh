cd5f9da870bf914a613edbb24b25c04bcfa031be
Luca Weiss <luca@z3ntu.xyz>
2020-10-18 12:49:49 +0200

    update for other source trees

--- boot-from-sourcetree.sh
+++ boot-from-sourcetree.sh
@@ -1,13 +1,12 @@
 #!/bin/bash -ex
 
-if [ $(basename $PWD) != "linux" ]; then
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+if [ ! -f arch/arm/boot/zImage ]; then
     echo "Please run from the linux source tree with arch/arm/boot/zImage"
     exit 1
 fi
 
-cat arch/arm/boot/zImage arch/arm/boot/dts/qcom-msm8974-fairphone-fp2.dtb > ../zImage-dtb
-cd ..
-./make_bootimg.sh
-cd out
-sign_img mainline-boot.img
-echo fastboot flash boot mainline-boot.img
+cat arch/arm/boot/zImage arch/arm/boot/dts/qcom-msm8974-fairphone-fp2.dtb > arch/arm/boot/zImage-dtb
+"$DIR"/make_bootimg.sh
+echo fastboot flash boot out/mainline-boot.img
--- make_bootimg.sh
+++ make_bootimg.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -x
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+mkdir -p out/
 rm out/*
 
 # 'rdinit=/init' - run /init from the initramfs
@@ -8,8 +11,7 @@ rm out/*
 # 'drm.debug=31' - verbose logging in drm drivers
 # 'clk_ignore_unused pd_ignore_unused' - keep unused clocks and power domains enabled
 # 'PMOS_NO_OUTPUT_REDIRECT' - print postmarketOS ramdisk output to the serial console
-#cmdline='rdinit=/init earlycon=msm_serial_dm,0xf991e000 PMOS_NO_OUTPUT_REDIRECT clk_ignore_unused pd_ignore_unused cma=300m msm.vram=200m' # debug drm.debug=31'
-cmdline='rdinit=/init earlycon=msm_serial_dm,0xf991e000 PMOS_NO_OUTPUT_REDIRECT clk_ignore_unused pd_ignore_unused cma=500m msm.vram=400m' # debug drm.debug=31'
+cmdline='rdinit=/init earlycon=msm_serial_dm,0xf991e000 PMOS_NO_OUTPUT_REDIRECT clk_ignore_unused pd_ignore_unused cma=500m msm.vram=192m' # debug drm.debug=31'
 mkbootimg \
     --base 0 \
     --pagesize 2048 \
@@ -18,6 +20,6 @@ mkbootimg \
     --second_offset 0x00f00000 \
     --tags_offset 0x00000100 \
     --cmdline "$cmdline" \
-    --kernel zImage-dtb \
-    --ramdisk ramdisk.cpio.gz \
+    --kernel arch/arm/boot/zImage-dtb \
+    --ramdisk "$DIR"/ramdisk.cpio.gz \
     -o out/mainline-boot.img
