dc788b1cc0404c8c6a93c75bbd94518559e9f748
Luca Weiss <luca@z3ntu.xyz>
2021-08-14 22:03:07 +0200

    Read cmdline from file
    This avoids hardcoding the cmdline into the script

--- make_bootimg.sh
+++ make_bootimg.sh
@@ -9,16 +9,8 @@ device="$1"
 pmaports_dir="$(pmbootstrap config aports)"
 source "$pmaports_dir"/device/*/device-"$device"/deviceinfo
 
-# 'rdinit=/init' - run /init from the initramfs
-# 'earlycon=msm_serial_dm,0xf991e000' - print output to the serial console as soon as possible
-# 'cma=64m msm.vram=16m' - allocate some memory for the display, should be gone once iommu works
-# 'drm.debug=31' - verbose logging in drm drivers
-# 'clk_ignore_unused pd_ignore_unused' - keep unused clocks and power domains enabled
-# 'PMOS_NO_OUTPUT_REDIRECT' - print postmarketOS ramdisk output to the serial console
-cmdline='rdinit=/init earlycon=msm_serial_dm,0xf991e000 PMOS_NO_OUTPUT_REDIRECT clk_ignore_unused pd_ignore_unused cma=500m msm.vram=192m msm.allow_vram_carveout=1' # debug drm.debug=31'
-cmdline='console=tty0 console=ttyMSM0,115200,n8 PMOS_NO_OUTPUT_REDIRECT deferred_probe_timeout=30 clk_ignore_unused pd_ignore_unused cma=500m msm.vram=192m msm.allow_vram_carveout=1'
-cmdline='rdinit=/init PMOS_NO_OUTPUT_REDIRECT clk_ignore_unused pd_ignore_unused'
-# TODO need better cmdline solution here - maybe file?
+# Read the cmdline for the device from a file
+cmdline=$(cat "$DIR/$device.cmdline")
 
 case "$deviceinfo_arch" in
     armv7)
