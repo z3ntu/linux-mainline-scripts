0c6e28b1a40629ea97ac5da450da749c16c5ade8
Luca Weiss <luca@z3ntu.xyz>
2022-07-23 12:09:33 +0200

    Add file parameter to mediawiki script

--- fp2-hw-support-to-mediawiki-to-clipboard.sh
+++ fp2-hw-support-to-mediawiki-to-clipboard.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-pandoc fp2-hw-support.md --from gfm --to mediawiki |
+if [ ! -f "$1" ]; then
+    echo "Usage: $0 file.md"
+    exit 1
+fi
+
+pandoc "$1" --from gfm --to mediawiki |
     sed 's/{|/{| class="wikitable"/' |
     sed 's|{DSP}|<ref name="DSP"/>|' |
     sed 's|{CCI}|<ref name="CCI"/>|' |
